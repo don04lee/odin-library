1f4bed3ceb4a67ba29e4b598796097311d63a0e6
Dong Woo Lee <don04lee@gmail.com>
2023-08-13 21:22:10 -0700

    Add popup transition for adding a new book

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+let shelf = document.getElementById('shelf');
+let newButton = document.getElementById('button');
+let popup = document.getElementById('popup');
+
 let myLibrary = [];
 
-function Book() {
+newButton.addEventListener('click', function() {
+  popup.classList.toggle('open-popup');
+});
 
+function Book(name, author, pages) {
+  this.name = name;
+  this.author = author;
+  this.pages = pages;
 }
 
-function addBookToLibrary() {
-  
+function addBookToLibrary(book) {
+
 }
